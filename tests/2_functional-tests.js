2cb11a40918b3332d89747a1978c0bb95277d4e6
Kate Mills <helloworldloveksm@icloud.com>
2022-02-11 19:15:22 -0800

    chai-http

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -26,8 +26,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
@@ -36,18 +36,47 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
+        .send({surname: 'Colombo'})
 
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(
+            res.body.name,
+            'Cristoforo',
+            'res.body.name should be "Christoforo"'
+          );
+          assert.equal(
+            res.body.surname,
+            'Colombo',
+            'res.body.surname should be "Colombo"'
+          );
 
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({name: 'Giovanni', surname: 'da Verrazzano'})
 
-      done();
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(
+            res.body.name,
+            'Giovanni',
+            'res.body.name should be "Giovanni"'
+          );
+          assert.equal(
+            res.body.surname,
+            'da Verrazzano',
+            'res.body.surname should be "da Verazzano"'
+          );
+          done();
+        })
     });
   });
 });
